539ce8845d536cbffad4507a721d0a41326ea2b8
Ninagawa123 <8329123+Ninagawa123@users.noreply.github.com>
2022-10-29 12:03:06 +0900

    Update Meridian.cpp

--- src/Meridian.cpp
+++ src/Meridian.cpp
@@ -1,1 +1,21 @@
-// cpp
+/*Meridian.h
+  This is a library to drive a communication system for humanoid robots.
+  It operates numerous servo motors and 9-axis sensors at 100 Hz and links status information with a PC in real time.
+  Created by Ninagawa123. October 29, 2022.
+  MIT license.*/
+
+#ifndef Meridian_h
+#define Meridian_h
+#include "Arduino.h"
+
+class checksum_val
+{
+  public:
+    checksum_val(short arr[], int len);
+    short arr[];
+    int len;
+  private:
+    int cksm;
+};
+
+#endif
