cef6abf17366863b4e1b3c3857dd8902b3f70e7d
Ninagawa123 <8329123+Ninagawa123@users.noreply.github.com>
2022-10-29 13:36:53 +0900

    Update Meridian.cpp

--- src/Meridian.cpp
+++ src/Meridian.cpp
@@ -1,29 +1,6 @@
 #include "arduino.h"
 #include "Meridian.h"
 
-// 初期化処理を行う関数
-// コンストラクタは　クラス名::クラス名と同じ名前で構成します
-LightingControl::LightingControl(int PinNo)
-{
-     pinMode(PinNo, OUTPUT) ;        // 指定されたデジタルピン番号をＬＥＤ出力に設定
-     LED_PinNo = PinNo ;
-}
-
-// ＬＥＤを点灯する関数(メソド関数)
-// クラス名::関数名() で構成します
-void LightingControl::On()
-{
-     digitalWrite(LED_PinNo, HIGH) ; // LEDを点灯する
-}
-
-
-
-
-// ＬＥＤを消灯する関数(メソド関数)
-void LightingControl::Off()
-{
-     digitalWrite(LED_PinNo, LOW) ;  // LEDを消灯する
-
 // +----------------------------------------------------------------------
 // | 関数名　　:  checksum_val(short arr[], int len)
 // +----------------------------------------------------------------------
@@ -43,19 +20,3 @@ short Meridian::checksum_val(short arr[], int len)
     return short(~cksm);
     // return ~cksm;//★もとの式　動いたら消す
 }
-
-
-
-// クラスの定義
-// クラス名・コンストラクタ名・関数名や使用する変数名を定義します。
-class LightingControl
-{
-  public:
-         LightingControl(int PinNo) ;
-    void On() ;
-    void Off() ;
-  private:
-    int LED_PinNo ;    // LEDの接続されているピン番号を保存する変数
-} ;
-
-#endif
