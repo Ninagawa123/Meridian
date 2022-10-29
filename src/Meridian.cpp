190b8608ce70a51e389f9ce9044e1a1fb44fc3be
Ninagawa123 <8329123+Ninagawa123@users.noreply.github.com>
2022-10-29 12:03:43 +0900

    Update Meridian.cpp

--- src/Meridian.cpp
+++ src/Meridian.cpp
@@ -1,21 +1,19 @@
-/*Meridian.h
-  This is a library to drive a communication system for humanoid robots.
-  It operates numerous servo motors and 9-axis sensors at 100 Hz and links status information with a PC in real time.
-  Created by Ninagawa123. October 29, 2022.
-  MIT license.*/
 
-#ifndef Meridian_h
-#define Meridian_h
-#include "Arduino.h"
-
-class checksum_val
+// 初期化処理を行う関数
+// コンストラクタは　クラス名::クラス名と同じ名前で構成します
+LightingControl::LightingControl(int PinNo)
 {
-  public:
-    checksum_val(short arr[], int len);
-    short arr[];
-    int len;
-  private:
-    int cksm;
-};
-
-#endif
+     pinMode(PinNo, OUTPUT) ;        // 指定されたデジタルピン番号をＬＥＤ出力に設定
+     LED_PinNo = PinNo ;
+}
+// ＬＥＤを点灯する関数(メソド関数)
+// クラス名::関数名() で構成します
+void LightingControl::On()
+{
+     digitalWrite(LED_PinNo, HIGH) ; // LEDを点灯する
+}
+// ＬＥＤを消灯する関数(メソド関数)
+void LightingControl::Off()
+{
+     digitalWrite(LED_PinNo, LOW) ;  // LEDを消灯する
+}
